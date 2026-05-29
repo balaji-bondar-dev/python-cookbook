717d9b84241d31bf784dad3f863f00d42934865c
Balaji Bondar <balajibondar@gmail.com>
2026-05-29 18:02:29 +0530

    Add data processing examples using pandas; create and save DataFrame to CSV, JSON, and Excel formats

--- README.md
+++ README.md
@@ -99,4 +99,26 @@ print(f"Sum: {result}")  # Output: Sum: 8
 
 
 
+# pandas is a powerful data manipulation library in Python. It provides data structures and functions needed to manipulate structured data seamlessly. In this example, we create a DataFrame with some sample data, including names, ages, cities, and today's date as a datetime object. We then convert the 'todays_datetime' column to a datetime format and print the DataFrame to see the results.
+import pandas as pd
+import os
+
+df = pd.DataFrame(
+    {
+        "Name": ["Alice", "Bob", "Charlie"],
+        "Age": [25, 30, 35],
+        "City": ["New York", "Los Angeles", "Chicago"],
+        "todays_datetime": pd.to_datetime("2024-06-01"),
+    }
+)
+
+df["todays_datetime"] = pd.to_datetime(df["todays_datetime"])
+
+
+# print(df)
+
+os.getcwd()
+
+data_path = "data/raw/sales-data.csv"
+os.path.exists(data_path)
 
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 # pandas is a powerful data manipulation library in Python. It provides data structures and functions needed to manipulate structured data seamlessly. In this example, we create a DataFrame with some sample data, including names, ages, cities, and today's date as a datetime object. We then convert the 'todays_datetime' column to a datetime format and print the DataFrame to see the results.
 import pandas as pd
+import os
 
-df = pd.DataFrame(
-    {
-        "Name": ["Alice", "Bob", "Charlie"],
-        "Age": [25, 30, 35],
-        "City": ["New York", "Los Angeles", "Chicago"],
-        "todays_datetime": pd.to_datetime("2024-06-01"),
-    }
+df = pd.read_csv("data/raw/sales-data.csv")
+print(df)
+
+# create output directory if it doesn't exist
+output_dir = "data/processed"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Save dataframe to json file
+df.to_json(
+    os.path.join(output_dir, "sales-data-processed.json"), orient="records", lines=True
 )
 
-df["todays_datetime"] = pd.to_datetime(df["todays_datetime"])
-print(df)
+# Save dataframe to csv file
+df.to_csv(os.path.join(output_dir, "sales-data-processed.csv"), index=False)
+
+# Save dataframe to excel file
+df.to_excel(os.path.join(output_dir, "sales-data-processed.xlsx"), index=False)
