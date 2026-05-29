c7ef7aa7725408f80d34b2f2a996b04f41139d07
Balaji Bondar <balajibondar@gmail.com>
2026-05-29 11:48:02 +0530

    updated folder structure

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# import requests module
-import requests
+def main():
+    print("Hello from my-new-project!")
 
-# Making a get request
-response = requests.get("https://api.github.com/")
 
-status_code = response.status_code
-print("Status Code:", status_code)
-
-# Store JSON data in API_Data
-API_Data = response.json()
-print(API_Data)
-
-# Print json data using loop
-for key in API_Data:
-    {print(key, ":", API_Data[key])}
+if __name__ == "__main__":
+    main()
